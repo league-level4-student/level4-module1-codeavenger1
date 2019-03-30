83408679d146afa5dd577307e7109779c6c97d1d
codeavenger1 <shivakansagara@gmail.com>
2019-03-29 18:33:56 -0700

    started level 4 mod 1

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,21 +3,35 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
+	Object array[];
 	
 	public ArrayList() {
+		array = new Object[1];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
-		return null;
+		return (T)array[loc];
 	}
 	
 	public void add(T val) {
-		
+		Object arr[] = new Object[array.length+1];
+		for (int i = 0; i < array.length; i++) {
+			arr[i]=array[i];
+		}
+		arr[array.length]=val;
+		array=arr;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		Object ins[] = new Object[array.length+1];
+		for (int i = 0; i < loc; i++) {
+			ins[i]=array[i];
+		}
+		ins[loc]=val;
+		for (int i = loc+1; i < array.length; i++) {
+			ins[i+1]=array[i+1];
+		}
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
